36a9c8638e095d95a7f905f21abbd6fd9b080cb1
emil.mekhtiyev <emil.mekhtiyev@outlook.com>
2025-09-19 18:01:32 +0500

    some events exercise

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -2,9 +2,23 @@ import { EventEmitter } from 'events';
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on('timeout', () => {
-    console.log('Timeout event');
+const timeoutListenerFn = (secondsQty) => {
+    console.log(`Timeout event in ${secondsQty} seconds`);
+};
+
+myEmitter.on('timeout', timeoutListenerFn);
+
+setTimeout(() => myEmitter.emit('timeout', 1), 1000);
+setTimeout(() => myEmitter.emit('timeout', 2), 2000);
+
+myEmitter.once('singleEvent', () => {
+    console.log('Single event');
 });
 
-setTimeout(() => myEmitter.emit('timeout'), 1000);
-setTimeout(() => myEmitter.emit('timeout'), 2000);
+// функция слушателя будет вызвано только один раз, из-за метода .once()
+setTimeout(() => myEmitter.emit('singleEvent'), 500);
+setTimeout(() => myEmitter.emit('singleEvent'), 1500);
+
+// отключение слушателя события "timeout"
+setTimeout(() => myEmitter.off('timeout', timeoutListenerFn), 3000);
+setTimeout(() => myEmitter.emit('timeout', 4), 4000);
